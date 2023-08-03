9a52e55c904e7adf34cb8cf1491ebca18a29aee4
Thanuka Sahan Pathirana <tsahan23@live.com>
2023-08-03 07:54:47 +0530

    Schhol_08_03 add password creator

---
+++ Application/Helper/CryptoHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class CryptoHelper
+{
+    // Hashing using SHA256
+    public static string GetSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+            string hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            return hash;
+        }
+    }
+
+    // Encrypting using AES
+    public static string EncryptAES(string input, string key, string iv)
+    {
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] ivBytes = Encoding.UTF8.GetBytes(iv);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = keyBytes;
+            aesAlg.IV = ivBytes;
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(input);
+                    }
+                }
+
+                byte[] encryptedBytes = msEncrypt.ToArray();
+                string encryptedText = Convert.ToBase64String(encryptedBytes);
+                return encryptedText;
+            }
+        }
+    }
+
+    // Decrypting using AES
+    public static string DecryptAES(string encryptedText, string key, string iv)
+    {
+        byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] ivBytes = Encoding.UTF8.GetBytes(iv);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = keyBytes;
+            aesAlg.IV = ivBytes;
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        string decryptedText = srDecrypt.ReadToEnd();
+                        return decryptedText;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Data/BaseClass/PasswordKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.BaseClass
+{
+    public class PasswordKey
+    {
+        public string key { get; set; } = "049A58D9-E7C7-469B-A6EA-773FD305";
+        public string iv { get; set; } = "3EFFE2A2-8044-4B";
+    }
+}
