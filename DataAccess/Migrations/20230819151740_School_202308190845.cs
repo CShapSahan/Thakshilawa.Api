3dc6ec2db903a36d4368bf80ba12f9101a183377
Thanuka Sahan Pathirana <tsahan23@live.com>
2023-08-21 07:45:56 +0530

    School_Api 2023--08-21 add DB Migrations

---
+++ DataAccess/Migrations/20230819151740_School_202308190845.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class School_202308190845 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "UserRoles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsActive",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 8, 19, 20, 47, 40, 837, DateTimeKind.Local).AddTicks(6631));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "UserRoles");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 8, 1, 16, 31, 54, 320, DateTimeKind.Local).AddTicks(3897));
+        }
+    }
+}
